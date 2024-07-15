d6ef65c82f32e346fd9614ed302f2028f1886beb
vib <vibrunazo@gmail.com>
2024-07-14 22:43:11 -0300

    41. Text, Strings and Colors

--- base/base.gd
+++ base/base.gd
@@ -2,16 +2,27 @@ class_name Base
 extends Node3D
 
 @export var max_health: int = 5
-var health: int = 5
+var health: int = 5:
+	set(new_health):
+		health = new_health
+		health = clampi(health, 0, max_health)
+		if label_3d: 
+			label_3d.text = "%s/%s" % [health, max_health]
+			label_3d.modulate = lerp(Color.RED, Color.WHITE, float(health)/float(max_health))
+		if health <= 0: die()
+
+var is_ready: bool = true
 
 @onready var label_3d: Label3D = $Label3D
 
 func _ready() -> void:
 	health = max_health
-	label_3d.text = str(health)
 
 func take_damage() -> void:
 	print('damage dealt to base')
 	health -= 1
-	health = clampi(health, 0, max_health)
-	label_3d.text = str(health)
+	
+func die() -> void:
+	if not is_ready: return
+	is_ready = false
+	get_tree().reload_current_scene()
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,10 +5,6 @@ extends PathFollow3D
 
 @onready var base: Base = get_tree().get_first_node_in_group("base")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
