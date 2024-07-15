29d59dab0d93ae1ce54a9913d4da579142e611d8
vib <vibrunazo@gmail.com>
2024-07-15 13:42:27 -0300

    43. Updating Gridmap Cells

--- camera/ray_picker_camera.gd
+++ camera/ray_picker_camera.gd
@@ -12,5 +12,12 @@ func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast.target_position = project_local_ray_normal(mouse_position) * 100
 	ray_cast.force_raycast_update()
-	print("Collider: %s, pos: %s" % [ray_cast.get_collider(), ray_cast.get_collision_point()])
+	if ray_cast.is_colliding():
+		var collider := ray_cast.get_collider()
+		if collider is GridMap:
+			var collision_point: Vector3 = ray_cast.get_collision_point()
+			var cell: Vector3i = collider.local_to_map(collision_point)
+			printt(cell, collider.get_cell_item(cell))
+			if collider.get_cell_item(cell) == 0:
+				collider.set_cell_item(cell, 1)
 	
