ae3834cad341d118503f95154ad4b79822b26ba8
vib <vibrunazo@gmail.com>
2024-07-16 23:41:02 -0300

    54. Control Nodes and UI

---
+++ ui/bank.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+@export var starting_gold: float = 150
+@onready var gold_label: Label = $GoldLabel
+
+var gold: float:
+	set(value):
+		gold = max(0, value)
+		if gold_label:
+			gold_label.text = "gold: %s" % gold
+
+func _ready() -> void:
+	gold = starting_gold
